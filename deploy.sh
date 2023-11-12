d7d8b81a7a720c679381f6f9d634f180b787f7e9
Ubuntu <ubuntu@ip-172-31-39-61.ap-south-1.compute.internal>
2023-11-12 08:23:45 +0000

    updated new code

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == origin/main ]]; then
-        echo "commit to main branch"
-	sh ./build.sh
 
-else [[ $GIT_BRANCH == origin/dev ]]
-        echo "commit to dev branch"
-	sh ./build.sh
+# Get the current Git branch
+git_branch=$(git rev-parse --abbrev-ref HEAD)
 
+# Set Docker image tag based on the Git branch
+if [ "$git_branch" == "origin/main" ]; then
+	echo "commit is made in main branch"
+       	docker_tag="main"
+elif [ "$git_branch" == "origin/dev" ]; then
+	echo "commit is made in dev branch"
+	docker_tag="dev"
+else
+    echo "Unsupported branch: $git_branch. Exiting."
+    exit 1
 fi
