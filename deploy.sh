7989f36b4b38b7d1fd814d44778599f159069a7c
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-12 23:53:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+dev_docker_pub_repo="manjushreegs/dev"
+prod_docker_pri_repo="manjushreegs/prod"
+
 # Get the current Git branch
-git_branch=$(git rev-parse --abbrev-ref HEAD)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Set Docker image tag based on the Git branch
-if [ "$git_branch" == "origin/main" ]; then
+if [ "$GIT_BRANCH" == "origin/main" ]; then
 	echo "commit is made in main branch"
        	docker_tag="main"
-elif [ "$git_branch" == "origin/dev" ]; then
+	
+elif [ "$GIT_BRANCH" == "origin/dev" ]; then
 	echo "commit is made in dev branch"
 	docker_tag="dev"
+ 	docker build -t "$dev_docker_pub_repo:$docker_tag" .
+ 
 else
-    echo "Unsupported branch: $git_branch. Exiting."
+    echo "Unsupported branch: $GIT_BRANCH. Exiting."
     exit 1
 fi
