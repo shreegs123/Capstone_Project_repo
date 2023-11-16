98ec6e31018c905a6086bdcb3c361727bb9a6893
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-17 00:09:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ dev_docker_pub_repo="manjushreegs/dev"
 prod_docker_pri_repo="manjushreegs/prod"
 
 # Get the current Git branch
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+GIT_BRANCH=$(git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)")
 echo "$GIT_BRANCH"
